0768c4708c06377272d18c972f6da37af2195be3
k-nagayama <k-nagayama@jreast.co.jp>
2023-04-26 02:38:58 +0000

    20230426-1138
    add instructions.md
    changed configy.py helpers.py

--- src/config.py
+++ src/config.py
@@ -10,6 +10,11 @@ class appProperties:
         image_dir: 画像ディレクトリのパス
         output_dir: 出力ディレクトリのパス
         camera_type: カメラ種類
+        camera_names: カメラ名称の辞書(HD11->高(左))
+        rail_names: 線区名称の辞書(Chuo->中央線)
+        station_names: 駅名称の辞書(Tokyo->東京)
+        rail_type_names: 線別の辞書(up->上り)
+        time_band_names: 時間帯の辞書(day->昼間)
     """
     def __init__(self, config_path):
         with open(config_path, 'r') as yml:
@@ -27,13 +32,45 @@ class appProperties:
     @property
     def output_dir(self):
         return self.config['default']['output_dir']
+    
+    @property
+    def readme_md(self):
+        return self.config['default']['readme_md']
 
     @property
     def camera_types(self):
         return self.config['default']['camera_types']
+    
+    @property
+    def camera_names(self):
+        return self.config['format_func']['camera_names']
+    
+    @property
+    def rail_names(self):
+        return self.config['format_func']['rail_names']
+    
+    @property
+    def station_names(self):
+        return self.config['format_func']['station_names']
+    
+    @property
+    def rail_type_names(self):
+        return self.config['format_func']['rail_type_names']
+    
+    @property
+    def time_band_names(self):
+        return self.config['format_func']['time_band_names']
 
 
 if __name__ == "__main__":
     config = appProperties('config.yml')
+    print(config.image_dir)
+    print(config.output_dir)
+    print(config.readme_md)
     print(config.camera_types)
+    print(config.camera_names)
+    print(config.rail_names)
+    print(config.station_names)
+    print(config.rail_type_names)
+    print(config.time_band_names)
     print(config.missing_count_limit)
--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,10 @@
 import os
 import glob
+import re
+import datetime
+import pandas as pd
 import streamlit as st
+from pathlib import Path
 from PIL import Image
 
 
@@ -21,6 +25,34 @@ def list_images(target_dir):
 
 
 @st.cache
+def get_dir_list(path):
+    dir_path = Path(path)
+    dir_obj_list = [path for path in dir_path.glob("*") if path.is_dir() and not path.name.startswith(".")]
+    dir_list = [image_obj.name for image_obj in dir_obj_list]
+    dir_list.sort()
+    return dir_list
+
+
+@st.cache
+def get_file_list(path):
+    dir_path = Path(path)
+    image_obj_list = [path for path in dir_path.glob("*") if path.is_file()]
+    image_list = [image_obj.name for image_obj in image_obj_list]
+    image_list.sort()
+    return image_list
+
+
+@st.cache
+def read_markdown_file(markdown_file):
+    return Path(markdown_file).read_text()
+
+
+@st.cache
+def read_python_file(python_file):
+    return Path(python_file).read_text()
+
+
+@st.cache
 def get_file_content_as_string(filename):
     st.code(filename)
 
@@ -31,3 +63,44 @@ def ohc_image_load(base_images, idx, caption):
     im_base = Image.open(base_images[idx])
     st.image(im_base, caption=caption)
     return im_base
+
+
+@st.cache
+def rail_name_to_jp(rail_name, config):
+    return config.rail_names[rail_name]
+
+
+@st.cache
+def station_name_to_jp(st_name, config):
+    return config.station_names[st_name]
+
+
+@st.cache
+def rail_type_to_jp(rail_type, config):
+    return config.rail_type_names[rail_type]
+
+
+@st.cache
+def rail_type_name_to_jp(time_band_names, config):
+    return config.time_band_names[time_band_names]
+
+
+@st.cache
+def camera_num_to_name(camera_num, config):
+    return config.camera_names[camera_num]
+
+
+@st.cache
+def rail_message(dir_area, config):
+    str_list = re.split('[_-]', dir_area)
+    rail_name = rail_name_to_jp(str_list[0], config)
+    if str_list[3] == 'St':
+        st_name = station_name_to_jp(str_list[2], config) + '構内'
+    else:
+        st_name = station_name_to_jp(str_list[2], config) + '～' + station_name_to_jp(str_list[3], config)
+    updown_name = rail_type_to_jp(str_list[4], config)
+    date_obj = datetime.datetime.strptime(str_list[5], "%Y%m%d")
+    measurement_date = date_obj.strftime("%Y年%m月%d日")    # # yyyy年mm月dd日形式の文字列に変換
+    measurement_time = rail_type_name_to_jp(str_list[6], config)
+    return rail_name, st_name, updown_name, measurement_date, measurement_time
+
